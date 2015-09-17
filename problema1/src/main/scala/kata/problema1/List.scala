7f71afadde871c23e8ed6afbf74b176a1d1a8353
Sotirca Mihaita George <sotirca.mihai@gmail.com>
2015-09-17 23:15:32 +0300

    v.2

--- problema1/src/main/scala/kata/problema1/List.scala
+++ problema1/src/main/scala/kata/problema1/List.scala
@@ -11,20 +11,12 @@ sealed trait List[+T] {
   def map[U](f: T => U): List[U] = {
     @tailrec def mapHelper(f: T => U, lT: List[T], lU: List[U]): List[U] = {
 	  lT match {
-	    /* case Cons(h, Nil) => Cons(f(h), lU) */
 		case Cons(h, t) => mapHelper(f, t, Cons(f(h), lU))
 	    case Nil => lU
 	  }
 	}
 	
 	mapHelper(f, this, Nil).reverse
-  
-    /*
-    this match {
-	  case Cons(h, t) => Cons(f(h), t.map(f))
-	  case Nil => Nil
-	}
-	*/
   }
 
   /**
@@ -32,14 +24,30 @@ sealed trait List[+T] {
    * la Liste de elemente de U, returnează o Listă
    * de elemente de tip U.
    */
-  def flatMap[U](f: T => List[U]): List[U] = ???
+  def flatMap[U](f: T => List[U]): List[U] = this.map(f).flatten
 
   /**
    * Dacă Lista sursă este o Listă de Liste cu elemente
    * de tip U, atunci concatenează Listele și returnează
    * rezultatul.
    */
-  def flatten[U](implicit ev: T <:< List[U]): List[U] = ???
+  def flatten[U](implicit ev: T <:< List[U]): List[U] = {
+    def append(xs: List[U], ys: List[U]): List[U] = {
+	  xs match {
+	    case Cons(h, t) => Cons(h, append(t, ys))
+		case Nil => ys
+	  }
+	}
+	
+	@tailrec def flattenHelper(l: List[T], lF: List[U]): List[U] = {
+	  l match {
+	    case Cons(h, t) => flattenHelper(t, append(h, lF))
+	    case Nil => lF
+	  }
+	}
+	
+	flattenHelper(this, Nil).reverse
+  }
 
   /**
    * Dându-se o funcție predicat ce returnează adevăra sau
@@ -47,7 +55,19 @@ sealed trait List[+T] {
    * conține doar elementele pentru care predicatul a fost
    * adevărat.
    */
-  def filter(p: T => Boolean): List[T] = ???
+  def filter(p: T => Boolean): List[T] = {
+    @tailrec def filterHelper(p: T => Boolean, l: List[T], lF: List[T]): List[T] = {
+	  l match {
+	    case Cons(h, t) => {
+		  val tlF = if (p(h)) { Cons(h, lF) } else { lF }
+		  filterHelper(p, t, tlF)
+		}
+	    case Nil => lF
+	  }
+	}
+	
+	filterHelper(p, this, Nil).reverse
+  }
 
   /**
    * Returnează Lista sursă cu elementele în ordine inversă.
@@ -76,13 +96,27 @@ sealed trait List[+T] {
   /**
    * Adaugă la sfârșit elementul dat.
    */
-  def :+[U >: T](elem: U): List[U] = ???
+  def :+[U >: T](elem: U): List[U] = {
+    (elem :: this.reverse).reverse
+  }
 
   /**
    * Parcurge Lista de la cap la coadă și apelează
    * funcția dată pentru fiecare element.
    */
-  def foreach[U](f: T => U): Unit = ???
+  def foreach[U](f: T => U): Unit = {
+    @tailrec def foreachHelper(items: List[T]) {
+	  items match {
+	    case Cons(h, t) => {
+		  f(h)
+		  foreachHelper(t)
+		}
+		case Nil => {}
+	  }
+	}
+  
+    foreachHelper(this)
+  }
 }
 
 case class Cons[+T](head: T, tail: List[T]) extends List[T]
